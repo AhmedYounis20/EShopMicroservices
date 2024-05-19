b4ca78f7054932792a152f31890d325382eac53c
AhmedYounis20 <ahmed.m.younis20@gmail.com>
2024-05-19 16:15:04 +0300

    add Catalog Service (vertical silce arch)

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name,List<string> Category,string Descritpion,string ImagePath, decimal Price) : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+public class CreateProductHandler : ICommandHandler<CreateProductCommand,CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // create Product entity from command object
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            ImageFile = command.ImagePath,
+            Description = command.Descritpion,
+            Price = command.Price,
+        };
+        
+        // save to database ToDo
+        await Task.CompletedTask;
+
+        // return CreateProductResult result
+        return new CreateProductResult(Guid.NewGuid());
+
+    }
+}
