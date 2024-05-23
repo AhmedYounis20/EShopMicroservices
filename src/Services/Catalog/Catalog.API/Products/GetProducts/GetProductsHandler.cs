dec5e004ddceea1f1f0aea52fd8297f853d7bbcf
AhmedYounis20 <ahmed.m.younis20@gmail.com>
2024-05-23 06:38:50 +0300

    impelement Get Products endpoint

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,6 +1,15 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿
+
+namespace Catalog.API.Products.CreateProduct;
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsResult(IEnumerable<Product> Products);
+
+public class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
-    public class GetProductsHandler
+    public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
     {
+        logger.LogInformation("GetProductsQueryHandler.Handle called with {@request}", request);
+        var products  = await session.Query<Product>().ToListAsync(cancellationToken);
+        return new GetProductsResult(products);
     }
 }
